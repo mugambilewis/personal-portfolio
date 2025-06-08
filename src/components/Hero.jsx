9c963eeedc749175f0db0bbcda2511fbdb8b3c3c
Mugambi Lewis <mugambilewis001@gmail.com>
2025-06-09 02:30:53 +0300

    I added projects Cards in Mechanical Page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,51 +23,51 @@ const Hero = () => {
   return (
     <section 
     id="hero"
-    className={`w-screen md:min-h-[calc(100vh-5rem)] flex flex-col md:flex-row items-center justify-center text-center md:text-left px-6 pb-4 md:px-16  ${
+    className={`w-screen md:min-h-[calc(100vh-5rem)] flex flex-col md:flex-row items-center justify-center text-center md:text-left px-6 pb-4 md:px-16 gap-6  ${
          theme === 'dark' 
         ? 'bg-gray-950 text-white border-gray-700'
         : 'bg-white text-gray-800 border-gray-200'
     
     }`}>
-
-      <div className="w-3/4 md:w-1/3 flex justify-center my-2">
-        <img
-          src="/images/profile.png"
-          alt="Mugambi Lewis Profile picture"
-          className="w-[clamp(150px, 40vw, 300px)] max-w-full h-auto rounded-lg shadow-lg transition-transform duration-300 hover:scale-105"
-        />
-      </div>
-
-      <div className=" w-full md:w-2/3 md:mt-2 space-y-6">
-        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold  font-['Playfair_Display']">
-        Hi, I&apos;m Mugambi Lewis
-        </h1>
-        <h2 className="text-lg sm:text-2xl md:text-3xl lg:text-4xl font-bold ">
-          I&apos;m a <span ref={typedRef} className="text-blue-400"></span>
-        </h2>
-        <p className="text-lg sm:text-lg md:text-xl lg:text-2xl italic font-semibold  mt-2">
-        “Where engineering meets creativity—turning concepts into reality with precision and passion.&ldquo;</p>
-        <p className="mt-4 text-base  md:text-lg lg:text-lg">
-        Bringing Ideas to Life with Design and Innovation.
-I’m a passionate Mechanical Engineer, Front-End Developer, and Graphics Designer who loves creating things that stand out. Whether it&apos;s engineering smart solutions, building stunning websites, or designing eye-catching visuals, I turn ideas into reality with creativity and precision. Every project I work on is driven by innovation, quality, and attention to detail, ensuring the best results every time. Let’s build something amazing together!
-        </p>
-        <div className="mt-8 flex  justify-center md:flex-row   md:justify-start gap-4">
-          <button
-            className={`inline-block px-8 py-2  rounded-[0.375rem] text-[#60a5fa] text-base border-[2px] border-[#60a5fa] cursor-pointer transition duration-300 ease-in-out hover:bg-[#60a5fa] hover:text-white
-              ${theme === 'dark' ? 'dark:hover:bg-blue-500 dark:hover:text-white' : 'hover:bg-blue-500 hover:text-white'}`}
-              ><a href="#contact">Contact Me</a>
-          </button>
-         
-          <button
-            className="inline-block px-8 py-2 bg-white rounded-[0.375rem] text-[#60a5fa] text-base border-[2px] border-[#60a5fa] cursor-pointer transition duration-300 ease-in-out hover:bg-[#60a5fa] hover:text-white"
-            onClick={() => window.location.href = 'mailto:mugambilewis001@gmail.com'}>Hire Me
-          </button>
-          
-          
-          
+     
+        <div className="w-3/4 md:w-2/5 flex      justify-center my-2">
+          <img
+            src="/images/profile.png"
+            alt="Mugambi Lewis Profile picture"
+            className="w-[clamp(150px, 40vw, 300px)] max-w-full h-auto rounded-lg shadow-lg transition-transform duration-300 hover:scale-105"
+          />
         </div>
-      </div>
-  
+
+        <div className=" w-full md:w-3/5 md:mt-2 space-y-6">
+          <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold  font-['Playfair_Display']">
+          Hi, I&apos;m Mugambi Lewis
+          </h1>
+          <h2 className="text-lg sm:text-2xl md:text-3xl lg:text-4xl font-bold ">
+            I&apos;m a <span ref={typedRef} className="text-blue-400"></span>
+          </h2>
+          <p className="text-lg sm:text-lg md:text-xl lg:text-2xl italic font-semibold  mt-2">
+          “Where engineering meets creativity—turning concepts into reality with precision and passion.&ldquo;</p>
+          <p className="mt-4 text-base  md:text-lg lg:text-lg">
+          Bringing Ideas to Life with Design and Innovation.
+  I’m a passionate Mechanical Engineer, Front-End Developer, and Graphics Designer who loves creating things that stand out. Whether it&apos;s engineering smart solutions, building stunning websites, or designing eye-catching visuals, I turn ideas into reality with creativity and precision. Every project I work on is driven by innovation, quality, and attention to detail, ensuring the best results every time. Let’s build something amazing together!
+          </p>
+          <div className="mt-8 flex  justify-center md:flex-row   md:justify-start gap-4">
+            <button
+              className={`inline-block px-8 py-2  rounded-[0.375rem] text-[#60a5fa] text-base border-[2px] border-[#60a5fa] cursor-pointer transition duration-300 ease-in-out hover:bg-[#60a5fa] hover:text-white
+                ${theme === 'dark' ? 'dark:hover:bg-blue-500 dark:hover:text-white' : 'hover:bg-blue-500 hover:text-white'}`}
+                ><a href="#contact">Contact Me</a>
+            </button>
+          
+            <button
+              className="inline-block px-8 py-2 bg-white rounded-[0.375rem] text-[#60a5fa] text-base border-[2px] border-[#60a5fa] cursor-pointer transition duration-300 ease-in-out hover:bg-[#60a5fa] hover:text-white"
+              onClick={() => window.location.href = 'mailto:mugambilewis001@gmail.com'}>Hire Me
+            </button>
+            
+            
+            
+          </div>
+        </div>
+      
 </section>
 
   );
--- src/pages/Mechanical.jsx
+++ src/pages/Mechanical.jsx
@@ -1,15 +1,25 @@
-function Mechanical() {
+import PagesCard from '../PagesCard';
+import { useTheme } from '../useTheme';
+
+
+const Mechanical = () => {
+  const { theme } = useTheme();
     return (
-      <div className=" service-details flex flex-col items-center min-h-[calc(100vh-7rem)] w-[95%] md:w-[90%] lg:w-[80%] mx-auto mt-8">
-        <h2 className="text-3xl md:text-5xl font-bold text-gray-900 ">Mechanical Engineering</h2>
+      <div className={`service-details flex flex-col items-center min-h-[calc(100vh-7rem)] w-[90%] md:w-[85%] lg:w-[80%] mx-auto mt-8 &${
+         theme === 'dark' 
+        ? 'bg-gray-950 text-white border-gray-700'
+        : 'bg-white text-gray-800 border-gray-200'
+    
+      }`} >
+         <h3 className="text-2xl md:text-3xl font-semibold text-gray-800 ">Mechanical Engineering</h3>
         <div className="flex flex-col md:flex-row justify-between w-full mt-6 gap-6">
           <div className="w-full md:w-3/5 md:mt-2 space-y-6">
-            <h3 className="text-2xl md:text-3xl font-semibold text-gray-800 dark:text-gray-200">
+            <h3 className="text-xl md:text-2xl font-semibold text-gray-800 ">
               Turning Imagination into Engineered Reality</h3>
-            <p className="text-lg md:text-xl text-gray-700 dark:text-gray-300 mt-4">
+            <p className="text-lg md:text-xl text-gray-700  mt-4">
               Driven by precision and innovation, I specialize in designing efficient mechanical systems that solve real-world problems. With strong expertise in CAD (Computer-Aided Design), I bring concepts to life through detailed 3D models and technical drawings. From prototyping sustainable energy solutions to crafting smart hydraulic mechanisms, my engineering approach blends creativity, accuracy, and functionality. Every project I take on reflects my passion for turning technical ideas into impactful, real-life applications.
             </p>
-             <h3 className="text-xl md:text-3xl font-semibold text-gray-800 dark:text-gray-200">
+             <h3 className="text-xl md:text-3xl font-semibold text-gray-800 ">
               Key Achievements</h3>
           </div>
           <div className="w-full md:w-2/5 mt-6">
@@ -19,8 +29,32 @@ function Mechanical() {
               className="w-full h-auto rounded-lg shadow-lg mt-4" />
           </div>
         </div>
+        <h3 className="text-2xl md:text-3xl font-semibold text-gray-800 ">
+              My projects</h3>
         <div>
-
+          
+          <p className="text-lg md:text-xl text-gray-700 mt-4">
+            A collection of innovative mechanical solutions engineered with precision, creativity, and real-world impact.</p>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
+            <PagesCard
+              title="Hydraulic System Design"
+              description="Engineered a hydraulic system for efficient material handling, enhancing operational efficiency by 30%."
+              image="/images/solarcharger.jpg"
+              link="/hydraulic-system-design"
+            />
+            <PagesCard
+              title="Sustainable Energy Solutions"
+              description="Developed a prototype for a solar-powered water pump, reducing energy costs by 40%."
+              image="/images/solarcharger.jpg"
+              link="/sustainable-energy-solutions"
+            />
+            <PagesCard
+              title="3D Printed Mechanical Parts"
+              description="Created custom 3D printed components for rapid prototyping, reducing lead time by 50%."
+              image="/images/3d-printed-parts.jpg"
+              link="/3d-printed-mechanical-parts"
+            />
+          </div>
         </div>
       </div>
     );
